62ca57a9e0e6e2b7d8d40e11591df1fc25770ef9
Naime Ahmed <naime1947@gmail.com>
2020-01-01 00:24:18 +0600

    Project Created, Drink and Category model added with InterfaceRepostity

---
+++ Drinkstore/Data/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,15 @@
+﻿using Drinkstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drinkstore.Data.Interfaces
+{
+    public interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; set; }
+        IEnumerable<Drink> PreferedDrinks { get; set; }
+        Drink GetDrinkById(int drinkId);
+    }
+}
